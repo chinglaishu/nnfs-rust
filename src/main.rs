fd0c499586030d6be82417ddc0bcb724bfa88ef9
David Lai <david.lai@littauer.io>
2022-03-21 00:01:57 +0800

    [doc] add doc, and add parameter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::env;
 
 use constant::DATA_SIZE;
 
@@ -22,14 +23,15 @@ use std::io::Write;
 
 fn main() {
 
-  // let mut data_vec_vec_x: Vec<Vec<f32>> = vec![vec![0.9, 0.88, 0.05, 0.02], vec![0.1, 0.05, 0.95, 0.87]];
-  // let mut data_vec_y: Vec<usize> = vec![0, 1];
+  let args: Vec<String> = env::args().collect();
+  let is_save = args.contains(&String::from("save"));
+  let is_load = args.contains(&String::from("load"));
+  let is_train = args.contains(&String::from("train"));
 
   let mut data_vec_vec_x: Vec<Vec<f32>> = vec![vec![0.0; constant::DATA_SIZE]; constant::TOTAL_SAMPLE_NUM];
   let mut data_vec_y: Vec<usize> = vec![0; constant::TOTAL_SAMPLE_NUM];
 
   vector::random_x_y(&mut data_vec_vec_x, &mut data_vec_y);
-
   vector::shuffle_x_y(&mut data_vec_vec_x, &mut data_vec_y);
 
   // println!("{:?}", data_vec_vec_x);
@@ -41,15 +43,6 @@ fn main() {
   let mut activation_relu_2 = activation::marco_create_activation_relu(BATCH_SIZE, DENSE_LAYER_2_NEURON_NUM);
   let mut layer_3 = dense_layer::create_dense_layer(2, DENSE_LAYER_2_NEURON_NUM, DENSE_LAYER_3_NEURON_NUM, 0.0, 0.0, 0.0, 0.0);
   let mut activation_softmax = activation::marco_create_activation_softmax(BATCH_SIZE, DENSE_LAYER_3_NEURON_NUM);
-  // activation_softmax.forward(&activatioin_relu_1.output_vec);
-
-  // println!("{:?}", activation_softmax.output_vec);
-  
-  // let mut predict_vec: Vec<usize> = vec![0; DATA_SIZE];
-
-  // activation_softmax.predict(&mut predict_vec);
-
-  // println!("{:?}", predict_vec);
 
   let mut loss = loss::create_loss_cross_entropy(BATCH_SIZE, DENSE_LAYER_3_NEURON_NUM);
   let mut optimizer = OptimizerAdam {
@@ -74,12 +67,17 @@ fn main() {
     predict_vec: vec![0; BATCH_SIZE],
   };
 
-  model.load_dense_layer();
+  if is_load {
+    model.load_dense_layer();
+  }
 
+  if is_train {
+    model.train(&data_vec_vec_x, &data_vec_y, 8, 1);
+  }
 
-  // model.train(&data_vec_vec_x, &data_vec_y, 8, 1);
-
-  // model.save_dense_layer();
+  if is_save {
+    model.save_dense_layer();
+  }
 
   let mut test_x: Vec<Vec<f32>> = vec![vec![0.0; constant::DATA_SIZE]; constant::BATCH_SIZE];
   let mut test_y: Vec<usize> = vec![0; constant::BATCH_SIZE];
@@ -89,86 +87,5 @@ fn main() {
   // println!("{:?}", test_x);
   // println!("{:?}", test_y);
 
-  // let result = utils_function::write_file();
-  // match result {
-  //   Err(err) => {
-  //     println!("err");
-  //   },
-  //   Ok(ok) => {
-  //     println!("ok");
-  //   }
-  // }
-  // return;
-
-
   model.test(&mut test_x, &test_y);
-
-  // println!("{}", constant::BATCH_SIZE);
-  // println!("Hello, world!");
-  // let mut data_arr: [[f32; constant::BATCH_SIZE]; constant::X_NUM] = [[0.0; constant::BATCH_SIZE]; constant::X_NUM];
-  // test_data::random_array_2d(&data_arr);
-  // println!("{:?}", data_arr);
-
-  // println!("{}", constant::BATCH_SIZE);
-  // println!("Hello, world!");
-  // let start_num: f32 = 0.0;
-  // let mut data_arr_arr_x = Vec::new();
-  // let mut arr_1 = vec![0.0; 5];
-  // let mut arr_2 = vec![1.0; 5];
-  // let mut arr_3 = vec![2.0; 5];
-  // println!("{:p}", &arr_1);
-  // println!("{:p}", &arr_2);
-  // data_arr_arr_x.push(&arr_1);
-  // data_arr_arr_x.push(&arr_2);
-  // data_arr_arr_x.push(&arr_3);
-  // println!("{:?}", data_arr_arr_x);
-  // test_data::print_vec_2d(data_arr_arr_x);
-  // get_data_arr_arr_x(&mut data_arr_arr_x);
-  // let mut data_arr_y = vec![0; constant::DATA_SIZE];
-  // test_data::random_x_y(&mut data_arr_arr_x, &mut data_arr_y);
-  // println!("{:?}", data_arr_arr_x);
-
-}
-
-fn back_main() {
-  let mut data_vec_vec_x: Vec<Vec<f32>> = vec![vec![0.9, 0.88, 0.05, 0.02], vec![0.1, 0.05, 0.95, 0.87]];
-  let mut data_vec_y: Vec<usize> = vec![0, 1];
-
-  println!("x: {:?}", data_vec_vec_x);
-
-  let layer_1 = &mut dense_layer::create_dense_layer(0, DATA_SIZE, DENSE_LAYER_1_NEURON_NUM, 0.0, 0.0, 0.0, 0.0);
-
-  match layer_1 {
-    ModelLayer::ModelDenseLayer(dense_layer) => {
-      dense_layer.forward(&data_vec_vec_x);
-      vector::copy_vector_2d_f32(&mut data_vec_vec_x, &dense_layer.output_vec);
-      println!("dense layer weight vec: {:?}", dense_layer.weight_vec);
-      println!("dense layer output: {:?}", dense_layer.output_vec);
-    },
-    _ => {}
-  }
-
-  // println!("layer 1: {:?}", layer_1);
-
-  let activation_softmax_1 = &mut activation::marco_create_activation_softmax(BATCH_SIZE, DENSE_LAYER_1_NEURON_NUM);
-
-  let mut predict_vec: Vec<usize> = vec![0; BATCH_SIZE];
-
-  match activation_softmax_1 {
-    ModelLayer::ModelActivationSoftmax(activation_softmax) => {
-      activation_softmax.forward(&data_vec_vec_x);
-      vector::copy_vector_2d_f32(&mut data_vec_vec_x, &activation_softmax.output_vec);
-      activation_softmax.predict(&mut predict_vec);
-      println!("softmax output: {:?}", activation_softmax.output_vec);
-    },
-    _ => {}
-  }
-
-  println!("activation softmax predict: {:?}", predict_vec);
-
-  let loss = &mut loss::create_loss_cross_entropy(BATCH_SIZE, 2);
-
-  let loss_mean = loss.loss_calculate(&data_vec_vec_x, &data_vec_y);
-  
-  println!("loss mean: {}", loss_mean);
 }
